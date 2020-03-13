901ba9d5c4820445c0cdd593393f233ccd1adb1c
skrishnachaitanya09 <39720489+skrishnachaitanya09@users.noreply.github.com>
2020-03-13 10:43:48 +0100

    lifi codes updates

--- LIFI/LIFI_RECEIVER/LIFI_RECEIVER.ino
+++ LIFI/LIFI_RECEIVER/LIFI_RECEIVER.ino
@@ -10,57 +10,112 @@ void setup() {
   ambientReading = analogRead(SOLARPIN);
 }
 
-void loop() {
+void loop() 
+{
 // Serial.println(ambientReading);
   int reading = analogRead(SOLARPIN);
+   //Serial.println(reading);
   int bits[8];
-  int startbit[4];
-  int key[] = {1,0,1,0};
+  int startbit[8];
+  int key[]={1,0,1,1,1,0,1,0};
   int p = 0;
 
 //  // Listening for the start bit
- if (reading > ambientReading + THRESHOLD) {
- 
-  for (int j = 0; j < 4; j++){
-    if (analogRead(SOLARPIN) > ambientReading + THRESHOLD) {
-        
-        startbit[j] = 1;
-      }
-      else {
-        startbit[j] = 0;
-      }
-      delay(100);
-    }}
-  // compare startbit with key
-
-  for (int k = 0; k < 8; k++){
-     
-       if(startbit[k] == key[k])
-       p = 1;
-       break;       
-     
+  int j=0;
+  while(j<8) 
+  {
+    if (analogRead(SOLARPIN) > ambientReading + THRESHOLD) 
+     {
+      startbit[j] = 1;
      }
-
-     if (reading > ambientReading + THRESHOLD && p ==1){
-    //delay(500);
-    for (int i = 0; i<8 ; i++) {
-      if (analogRead(SOLARPIN) > ambientReading + THRESHOLD) {
+     else 
+     {
+       startbit[j] = 0;
+     }
+     if(startbit[j]==key[j]) 
+     {
+      j++;
+     }
+     else 
+     {
+      j=0;
+     }
+     delay(100);
+  }
+ if(j==8) 
+ {
+ Serial.println("Key matched");
+ Serial.println("Key is ");
+ for(int i =0;i<8;i++) 
+ {
+  Serial.print(startbit[i]);
+ }
+ Serial.println("Data is ");
+ for(int i =0;i<8;i++) 
+ {
+      if (analogRead(SOLARPIN) > ambientReading + THRESHOLD) 
+      {
         
         bits[i] = 1;
       }
-      else {
+      else 
+      {
         bits[i] = 0;
       }
-      delay(200);
-    } 
-      for (int i = 0; i < 8; i++) {
-       Serial.print(bits[i]);
-    }
-    Serial.println("");
+      delay(100);
     
-    }
+  Serial.print(bits[i]);
+ }
+ }
+ }
 
-    
-    
-  }
+//  for (int j = 0; j < 8; j++){
+//    if (analogRead(SOLARPIN) > ambientReading + THRESHOLD) {
+//        
+//        startbit[j] = 1;
+//      }
+//      else {
+//        startbit[j] = 0;
+//      }
+//      delay(100);
+//    }}
+  // compare startbit with key
+
+//  for (int k = 0; k < 8; k++)
+//    {
+//     
+//       if(startbit[k] == key[k])
+//       
+//       {
+//        p = 1;
+//       }
+//       else
+//       {
+//        p=0;
+//        break;
+//       }
+//              
+//     
+//     }
+
+//     if (reading > ambientReading + THRESHOLD && p ==1){
+//    //delay(500);
+//    for (int i = 0; i<8 ; i++) {
+//      if (analogRead(SOLARPIN) > ambientReading + THRESHOLD) {
+//        
+//        bits[i] = 1;
+//      }
+//      else {
+//        bits[i] = 0;
+//      }
+//      delay(100);
+//    } 
+//      for (int i = 0; i < 8; i++) {
+//       Serial.print(bits[i]);
+//    }
+//    Serial.println("");
+//    
+//    }
+
+  
   
--- LIFI/LIFI_TRANSMITTER/LIFI_TRANSMITTER.ino
+++ LIFI/LIFI_TRANSMITTER/LIFI_TRANSMITTER.ino
@@ -1,31 +1,41 @@
 #define LASERPIN 2
-
+#define activpin 3
 void setup() {
   // put your setup code here, to run once:
   pinMode(LASERPIN, OUTPUT);
+  pinMode(activpin, INPUT);
 
 }
 
 void loop() {
   // 10011010
+  int key[]={1,0,1,1,1,0,1,0};
   int bits[] = {1,0,0,1,1,0,1,0};
   
+  
   //start bit
- digitalWrite(LASERPIN, HIGH);
- delay(100);
- digitalWrite(LASERPIN, LOW);
- delay(100);
- digitalWrite(LASERPIN, HIGH);
- delay(100);
- digitalWrite(LASERPIN, LOW);
-
-
-  for (int i = 0; i < 8; i++) {
-    digitalWrite(LASERPIN, bits[i]);
-    delay(200);
+// digitalWrite(LASERPIN, HIGH);
+// delay(100);
+// digitalWrite(LASERPIN, LOW);
+// delay(100);
+// digitalWrite(LASERPIN, HIGH);
+// delay(100);
+// digitalWrite(LASERPIN, LOW);
+if(digitalRead(activpin)== HIGH)
+{
+ for (int i = 0; i < 8; i++) 
+  {
+    digitalWrite(LASERPIN, key[i]);
+    delay(100);
   }
 
-  digitalWrite(LASERPIN, LOW);
-
-  delay(500);
+  for (int j = 0; j< 8; j++) 
+  {
+    digitalWrite(LASERPIN, bits[j]);
+    delay(100);
+  }
+}
+//  digitalWrite(LASERPIN, LOW);
+//
+//  delay(500);
 }
