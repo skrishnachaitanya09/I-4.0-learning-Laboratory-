8f3f31290bdcf08c96bd2f3d0f11a2eb4a21cb5c
Srivamsi Malladi <srivamsi.malladi@st.ovgu.de>
2020-10-27 16:57:49 +0100

    Updated robotic arm code

--- Basic Test Codes/braccioTesting/braccioTesting.ino
+++ Basic Test Codes/braccioTesting/braccioTesting.ino
@@ -14,64 +14,46 @@ void setup() {
   digitalWrite(12, HIGH);
   Braccio.begin(SOFT_START_DISABLED);
 
-//  Braccio.ServoMovement (20, 90, 90, 90, 90, 90, 10);
-//  delay(3000);
-//  
-//  Braccio.ServoMovement (20, 90, 80, 0, 90, 90, 10);
-//  delay(1000);
-//  Braccio.ServoMovement (20, 90, 80, 0, 90, 90, 73);
-//  delay(1000);
-//  Braccio.ServoMovement (20, 90, 90, 45, 90, 90, 73);
-//  delay(1000);
-//  Braccio.ServoMovement (20, 0, 80, 45, 90, 90, 73);
-//  delay(1000);
-//  Braccio.ServoMovement (20, 0, 80, 0, 90, 90, 73);
-//  delay(1000);
-//  Braccio.ServoMovement (20, 0, 80, 0, 90, 90, 10);
-//
-//  delay(3000);
-//  Braccio.ServoMovement (20, 0, 90, 90, 90, 90, 10);
-
-
-
 }
 
 void loop() {
   /* Sink A to AGV start*/
   Braccio.ServoMovement (20, 90, 90, 60, 0, 90, 10); //AGV up + gripper open
-  delay(500);
+  delay(100);
   Braccio.ServoMovement (20, 0, 90, 60, 0, 90, 10);   //Sink A up + gripper open
-  delay(500);
-  Braccio.ServoMovement (20, 10, 100, 30, 0, 90, 10);   //Sink A down + gripper open
-  delay(500);
-  Braccio.ServoMovement (20, 10, 100, 30, 0, 90, 60);   //Sink A down + gripper close
-  delay(500);
-  Braccio.ServoMovement (20, 10, 100, 60, 0, 90, 60);   //Sink A up + gripper close
-  delay(500);
-  Braccio.ServoMovement (20, 90, 100, 60, 0, 90, 60); //AGV up + gripper close
-  delay(500);
-  Braccio.ServoMovement (20, 90, 60, 45, 0, 90, 60); //AGV down + gripper close
-  delay(500);
-  Braccio.ServoMovement (20, 90, 60, 45, 0, 90, 10); //AGV down + gripper open
-  delay(500);
+  delay(100);
+  Braccio.ServoMovement (20, 0, 120, 5, 25, 90, 10);   //Sink A down + gripper open
+  delay(100);
+  Braccio.ServoMovement (20, 0, 117, 5, 25, 90, 70);   //Sink A down + gripper close
+  delay(100);
+  Braccio.ServoMovement (20, 0, 120, 30, 25, 90, 70);   //Sink A up + gripper close
+  delay(100);
+  Braccio.ServoMovement (20, 95, 120, 30, 25, 90, 70); //AGV up + gripper close
+  delay(100);
+  Braccio.ServoMovement (20, 95, 70, 30, 25, 90, 70); //AGV down + gripper close
+  delay(100);
+  Braccio.ServoMovement (20, 95, 70, 30, 25, 90, 10); //AGV down + gripper open
+  delay(100);
+  Braccio.ServoMovement (20, 90, 90, 60, 0, 90, 10); //AGV up + gripper open
+  delay(100);
   /* Sink A to AGV end*/
 
   /* Sink B to AGV start*/
   Braccio.ServoMovement (20, 90, 90, 60, 0, 90, 10); //AGV up + gripper open
-  delay(500);
-  Braccio.ServoMovement (20, 172, 90, 60, 0, 90, 10);   //Sink B up + gripper open
-  delay(500);
-  Braccio.ServoMovement (20, 172, 95, 30, 10, 90, 10);   //Sink B down + gripper open
-  delay(500);
-  Braccio.ServoMovement (20, 172, 95, 30, 10, 90, 60);   //Sink B down + gripper close
-  delay(500);
-  Braccio.ServoMovement (20, 172, 90, 60, 0, 90, 60);   //Sink B up + gripper close
-  delay(500);
-  Braccio.ServoMovement (20, 80, 90, 60, 0, 90, 60); //AGV up + gripper close
-  delay(500);
-  Braccio.ServoMovement (20, 80, 60, 45, 0, 90, 60); //AGV down + gripper close
-  delay(500);
-  Braccio.ServoMovement (20, 80, 60, 45, 0, 90, 10); //AGV down + gripper open
-  delay(500);
+  delay(100);
+  Braccio.ServoMovement (20, 180, 90, 60, 0, 90, 10);   //Sink B up + gripper open
+  delay(100);
+  Braccio.ServoMovement (20, 180, 100, 30, 10, 90, 10);   //Sink B down + gripper open
+  delay(100);
+  Braccio.ServoMovement (20, 180, 100, 30, 10, 90, 60);   //Sink B down + gripper close
+  delay(100);
+  Braccio.ServoMovement (20, 180, 100, 60, 0, 90, 60);   //Sink B up + gripper close
+  delay(100);
+  Braccio.ServoMovement (20, 85, 100, 60, 0, 90, 60); //AGV up + gripper close
+  delay(100);
+  Braccio.ServoMovement (20, 95, 65, 40, 15, 90, 60); //AGV down + gripper close
+  delay(100);
+  Braccio.ServoMovement (20, 95, 65, 40, 15, 90, 10); //AGV down + gripper open
+  delay(100);
   /* Sink B to AGV end*/
 }
