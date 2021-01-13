d1057bcd01226fe232a58900c2ced719a26c7765
NaveenkumarPesaru <61833276+NaveenkumarPesaru@users.noreply.github.com>
2021-01-13 16:32:26 +0100

    Add files via upload

---
+++ final codes/IR_Transmitter/IR_Transmitter.ino
@@ -0,0 +1,39 @@
+/*
+   IRremote: IRsendDemo - demonstrates sending IR codes with IRsend
+   An IR LED must be connected to Arduino PWM pin 3.
+   Version 0.1 July, 2009
+   Copyright 2009 Ken Shirriff
+   http://arcfn.com
+*/
+
+#include <IRremote.h>
+
+
+char input = '0';
+IRsend irsend;
+
+void setup()
+{ 
+  pinMode(3,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(Serial.available()) {
+    input = Serial.read();
+  }
+  else {
+    input = '0';
+  }
+
+  if (input == 'a') {
+    Serial.println("Sending message a");
+    irsend.sendSony(0xa80, 12);
+    delay(40);
+  }
+  else if (input == 'b') {
+    Serial.println("Sending message b");
+    irsend.sendSony(0xa90, 12);
+    delay(40);
+  }
+}
